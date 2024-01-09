954aacba894f5329156e8347ef9b547e02f7a7ff
Kamal Sharma <kamalsharma25091994@gmail.com>
2024-01-10 01:17:46 +0530

    upto add category

---
+++ Pariksha/ExamPortal_frontend/examportal_frontend/src/Pages/AdminPages/AddQuiz.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SideNavbar } from '../../Components/SideNavbar'
+
+export const AddQuiz = () => {
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900 min-w-full max-w-screen-2xl  mx-auto  text-justify min-h-screen">
+    <SideNavbar/> 
+    <div className='max-w-6xl mx-auto p-4 text-xl font-bold bg-white border text-gray-700 dark:text-white' >
+     Add Quiz
+     </div>
+ </section>
+  )
+}
--- Pariksha/ExamPortal_frontend/examportal_frontend/src/Routes/AllRoutes.js
+++ Pariksha/ExamPortal_frontend/examportal_frontend/src/Routes/AllRoutes.js
@@ -5,11 +5,15 @@ import { Login } from "../Pages/Login"
 import { PageNotFound } from "../Pages/PageNotFound"
 import { Dashboard } from "../Pages/AdminPages/Dashboard"
 import { Profile } from "../Pages/Profile"
+import { Categories } from "../Pages/AdminPages/Categories"
+import { AddCategory } from "../Pages/AdminPages/AddCategory"
+import { Quizzes } from "../Pages/AdminPages/Quizzes"
+import { AddQuiz } from "../Pages/AdminPages/AddQuiz"
 
 
 export const AllRoutes = () => {
     return (
-        <main >
+        <main className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto min-h-screen">
             <Routes>
 
                 <Route path="/" element={<Home />}></Route>
@@ -18,6 +22,10 @@ export const AllRoutes = () => {
             
                 <Route path="/admin/" element={<Dashboard />}></Route>
                 <Route path="/admin/profile" element={<Profile />}></Route>
+                <Route path="/admin/categories" element={<Categories />}></Route>
+                <Route path="/admin/addCategory" element={<AddCategory />}></Route>
+                <Route path="/admin/quizzes" element={<Quizzes />}></Route>
+                <Route path="/admin/addQuiz" element={<AddQuiz />}></Route>
                 <Route path="*" element={<PageNotFound />}></Route>
 
             </Routes>
